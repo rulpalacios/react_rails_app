301d3616998db6e6d99b70312f64ae70cf327c59
Raul Palacios <palacios.raul02@gmail.com>
2021-07-13 22:37:20 -0500

    Add routes for create post

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'posts/:id', to: 'hello_world#index';
   get 'posts/:id/edit', to: 'hello_world#index';
+  get 'posts/:id', to: 'hello_world#index';
+  get 'posts/new', to: 'hello_world#index';
   root 'hello_world#index'
 end
