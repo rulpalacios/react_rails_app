3c3f199b0a5a85e41044c3ea60aa155c4ab2bf31
Raul Palacios <palacios.raul02@gmail.com>
2021-07-13 22:36:35 -0500

    Add posts react components

---
+++ app/javascript/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+import Routes from './Routes';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+        <hr />
+        </div>
+        <Routes />
+      </Router>
+    );
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+
+  get 'posts/:id', to: 'hello_world#index';
+  get 'posts/:id/edit', to: 'hello_world#index';
+  root 'hello_world#index'
 end
